2ce1c17175cb67720f19760ba5b53b879c61f1fe
eduardobono <100717780+eduardobono@users.noreply.github.com>
2022-03-14 17:38:17 +0100

    Add files via upload

---
+++ funciones/menu.py
@@ -0,0 +1,37 @@
+'''
+        ##############################################
+        ### Autor: Eduardo Bono Fecha: 11/03/2022  ###
+        ###    Menu de los ejercicios propuestos   ###
+        ##############################################
+'''
+
+# colores para el menu
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+morado = '\033[35m'
+cian = '\033[36m'
+blanco = '\033[37m'
+reset = '\033[0m'
+
+# impresion del menu por pantalla
+print(azul, "   ╔════════════════════════════════════════════════════════════════════╗")
+print(azul, "   ║", amarillo, "                Ejercicios propuestos para SE02                 ", azul, "║")
+print(azul, "   ╠══════════════════════╦══════════════════════╦══════════════════════╣")
+print(azul, "   ║", "  1 -", amarillo, "Ejercicio  1", azul, "║   2 -", amarillo, "Ejercicio  2",azul, "║   3 -", amarillo, "Ejercicio  3", azul, "║")
+print(azul, "   ╠══════════════════════╬══════════════════════╬══════════════════════╣")
+print(azul, "   ║", "  4 -", amarillo, "Ejercicio  4", azul, "║   5 -", amarillo, "Ejercicio  5",azul, "║   6 -", amarillo, "Ejercicio  6", azul, "║")
+print(azul, "   ╠══════════════════════╬══════════════════════╬══════════════════════╣")
+print(azul, "   ║", "  7 -", amarillo, "Ejercicio  7", azul, "║   8 -", amarillo, "Ejercicio  8",azul, "║   9 -", amarillo, "Ejercicio  9", azul, "║")
+print(azul, "   ╠══════════════════════╬══════════════════════╬══════════════════════╣")
+print(azul, "   ║", " 10 -", amarillo, "Ejercicio 10", azul, "║  11 -", amarillo, "Ejercicio 11",azul, "║  12 -", amarillo, "Ejercicio 12", azul, "║")
+print(azul, "   ╠══════════════════════╬══════════════════════╬══════════════════════╣")
+print(azul, "   ║", " 13 -", amarillo, "Ejercicio 13", azul, "║  14 -", amarillo, "Ejercicio 14",azul, "║  15 -", amarillo, "Ejercicio 15", azul, "║")
+print(azul, "   ╠══════════════════════╬══════════════════════╬══════════════════════╣")
+print(azul, "   ║", " 16 -", amarillo, "Ejercicio 16", azul, "║  17 -", amarillo, "Ejercicio 17",azul, "║  18 -", amarillo, "Ejercicio 18", azul, "║")
+print(azul, "   ╠══════════════════════╩══════════════════════╩══════════════════════╣")
+print(azul, "   ║", rojo ,"                           0 -", amarillo, "Salir                           ", azul, "║")
+print(azul, "   ╚════════════════════════════════════════════════════════════════════╝")
+# seleccion = input(verde + "\nPor favor, elige una opción: " + reset)
