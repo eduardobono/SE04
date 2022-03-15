1d1db8f80f0bd84400f3a4f5efa62d75cc94036e
eduardobono <100717780+eduardobono@users.noreply.github.com>
2022-03-15 19:06:39 +0100

    Add files via upload

--- ejercicios/Ejercicio17.py
+++ ejercicios/Ejercicio17.py
@@ -4,3 +4,19 @@
         ###               Ejercicio 17             ###
         ##############################################
 '''
+
+from funciones.funciones import *
+
+# variables
+intentos = 3
+
+for i in range(1, intentos +1):
+    password = input("Por favor introduce la contraseña: ")
+    result = passwd(password)
+    if result == 1:
+        print("Contraseña correcta!!!")
+        exit(0)
+    else:
+        print(" Contraseña incorrecta, por favor vuelva a introducirla")
+
+print ("Ha introducido demasiadas veces la contraseña incorrecta, se informa al Administrador")
